7cbb44cf84a53c9a756ef3302f4e0a9402b37d12
satya <sroutraykec@gmail.com>
2025-07-05 20:57:17 +0530

    add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreen()
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
